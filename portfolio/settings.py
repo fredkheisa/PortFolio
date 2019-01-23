6a3c3f557c84cc1115bfbab1289056c08608291c
Github User Name <you@example.com>
2019-01-23 10:43:36 +0300

    adding or rather installing bootstrap3 at this commit..

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'prof.apps.ProfConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
